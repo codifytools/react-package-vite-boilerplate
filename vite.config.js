e170d998c8bf072038e967681b772f64286ec843
Juan Pablo Mejia <juanpmd@hotmail.com>
2023-10-18 16:37:48 -0700

    add initial package layout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import * as pkg from './package.json';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: Object.keys(pkg.peerDependencies),
+    },
+  },
 })
