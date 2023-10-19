369ccc66b408006cd49df743b684fce84fef0a90
Juan Pablo Mejia <juanpmd@hotmail.com>
2023-10-19 00:03:48 -0700

    add storybook setup

---
+++ .storybook/Component.stories.js
@@ -0,0 +1,8 @@
+import { Component } from '../src/index.jsx';
+
+export default {
+  title: 'Component',
+  component: Component,
+};
+
+export const Example = {};
---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+const config = {
+  stories: ["./*.stories.js"],
+  framework: {
+    name: "@storybook/react-vite",
+  }
+};
+
+export default config;
